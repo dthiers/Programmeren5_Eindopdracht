a6d3357df7cd519e0e3c611dbcc671e88033c382
dthiers <dthiers@student.avans.nl>
2014-10-20 22:43:22 +0200

    Roomviews en controller gemaakt en werkend.

--- PROGCS05_Dion/PROGCS05_Dion/Controllers/RoomController.cs
+++ PROGCS05_Dion/PROGCS05_Dion/Controllers/RoomController.cs
@@ -1,4 +1,5 @@
-﻿using PROGCS05_Dion.Models;
+﻿using DomainModel;
+using PROGCS05_Dion.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -14,8 +15,67 @@ namespace PROGCS05_Dion.Controllers
             roomRepository = new RoomRepository();
         }
 
+        /*
+         * Show all rooms
+         * */
         public ActionResult ShowAllRooms() {
             return View(roomRepository.GetAll());
+        }
+
+        /*
+         * Show details for room by Id
+         * */
+        public ActionResult DetailsRoom(int id) {
+            var r_details = roomRepository.GetRoomByID(id);
+            return View(r_details);
+        }
+
+        /*
+         * Create new room
+         * */
+        public ActionResult CreateRoom() {
+            return View(roomRepository.Get());
+        }
+
+        [HttpPost]
+        [ActionName("CreateRoom")]
+        public ActionResult CreatRoomPost(Room room) {
+            if (room != null) { roomRepository.Create(room); }
+            return RedirectToAction("ShowAllRooms");
+        }
+
+        /*
+         * Edit room
+         * */
+        public ActionResult EditRoom(int id) {
+            var r_edit = roomRepository.GetRoomByID(id);
+            return View(r_edit);
+        }
+
+        [HttpPost]
+        [ActionName("EditRoom")]
+        public ActionResult EditRoom(Room room) {
+            if (room != null) { roomRepository.Update(room); }
+
+            return RedirectToAction("DetailsRoom", new { id = room.Id});
+        }
+
+        
+        /*
+         * Delete room by Id
+         * */
+        public ActionResult DeleteRoom(int id) {
+            var r_delete = roomRepository.GetRoomByID(id);
+            return View(r_delete);
+        }
+
+        [HttpPost]
+        [ActionName("DeleteRoom")]
+        public ActionResult DeleteRoomPost(int id) {
+            var r_delete = roomRepository.GetRoomByID(id);
+            if (r_delete != null) { roomRepository.Delete(r_delete); }
+
+            return RedirectToAction("ShowAllRooms");
         }
     }
 }
