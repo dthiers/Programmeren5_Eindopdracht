6fbd010e75c2c91112f2a457b9c59d5605cc64c6
dthiers <dthiers@student.avans.nl>
2014-10-20 04:13:36 +0200

    Kijken of github werkt, alleen een comment gemaakt

--- PROGCS05_Dion/PROGCS05_Dion/Controllers/BookingController.cs
+++ PROGCS05_Dion/PROGCS05_Dion/Controllers/BookingController.cs
@@ -74,6 +74,7 @@ namespace PROGCS05_Dion.Controllers
         /*
          * Show booking within period
          * Weltrusten
+         * Laatste edit... kijken of Git weer werkt
          * */
         public ActionResult SelectBookingPeriod() {
             return View();
