7219ad33162f33b7c2297757d2910009cb6e475f
raymondphua - Raymond Phua <chrphua@student.avans.nl>
2014-10-20 16:53:52 +0200

    ForeignKey

--- PROGCS05_Dion/DomainModel/Booking.cs
+++ PROGCS05_Dion/DomainModel/Booking.cs
@@ -1,12 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 
 namespace DomainModel
 {
-    public class Booking{
+    public class Booking
+    {
 
 
         [Key]
